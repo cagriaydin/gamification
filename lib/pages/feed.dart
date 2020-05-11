16fbfbb041a9c13b2ff8149bf1a3c37ec46b9cb5
onatcipli <onatcipli@outlook.com>
2020-05-11 23:34:55 +0300

    content_selector.dart widget added

--- lib/pages/feed.dart
+++ lib/pages/feed.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:yorglass_ik/models/content_option.dart';
+import 'package:yorglass_ik/widgets/content_selector.dart';
 
 class FeedPage extends StatefulWidget {
   final Function menuFunction;
+
   FeedPage({this.menuFunction});
 
   @override
@@ -9,6 +12,24 @@ class FeedPage extends StatefulWidget {
 }
 
 class _FeedPageState extends State<FeedPage> {
+  var _options = [
+    ContentOption(
+      title: "Duyurular",
+      count: 15,
+      isActive: true,
+    ),
+    ContentOption(
+      title: "Webinar",
+      count: 10,
+      isActive: false,
+    ),
+    ContentOption(
+      title: "B2B",
+      count: 10,
+      isActive: false,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,29 +64,10 @@ class _FeedPageState extends State<FeedPage> {
         padding: const EdgeInsets.only(top: 16),
         child: Column(
           children: <Widget>[
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Text("Duyurular"),
-                    Text("15 İleti"),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Text("Webinarlar"),
-                    Text("10 İleti"),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Text("B2B"),
-                    Text("10 İleti"),
-                  ],
-                )
-              ],
+            Flexible(
+              child: ContentSelector(
+                options: _options,
+              ),
             )
           ],
         ),
