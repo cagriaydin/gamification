d4152227708696fb8f232005be1fb1be5a6aead8
oguzakpinar <oguzakpinar2308@gmail.com>
2020-05-18 01:37:50 +0300

    drawer added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:yorglass_ik/pages/bottom_navigation.dart';
+import 'package:yorglass_ik/pages/suggestion.dart';
 import 'package:yorglass_ik/services/authentication-service.dart';
 import 'package:yorglass_ik/widgets/custom_drawer/custom_drawer.dart';
 
@@ -45,8 +46,8 @@ class _HomePageState extends State<HomePage> {
       drawerBuilder: Builder(builder: (context) {
         return SafeArea(
           child: Container(
-            width: (size.width / 3) * 1.5,
-            padding: EdgeInsets.only(top: 50,bottom: 50),
+            width: (size.width / 3) * 1.7,
+            padding: EdgeInsets.only(top: 50, bottom: 50),
             child: Column(
               mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -55,20 +56,21 @@ class _HomePageState extends State<HomePage> {
                 Column(
                   mainAxisSize: MainAxisSize.max,
                   mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
                     Container(
                       height: size.width / 4,
                       width: size.width / 4,
                       decoration: BoxDecoration(
                         border: Border.all(color: Colors.white, width: 2.0),
-                        borderRadius:
-                            new BorderRadius.all(new Radius.circular(60)),
+                        borderRadius: new BorderRadius.all(new Radius.circular(60)),
                         image: DecorationImage(
                           fit: BoxFit.fill,
-                          image: AuthenticationService.verifiedUser.image == null ? AssetImage("assets/default-profile.png") : MemoryImage(base64.decode(AuthenticationService.verifiedUser.image)),
+                          image: AuthenticationService.verifiedUser.image == null
+                              ? AssetImage("assets/default-profile.png")
+                              : MemoryImage(base64.decode(AuthenticationService.verifiedUser.image)),
                         ),
-                        ),
+                      ),
                     ),
                     SizedBox(height: 16),
                     Text(
@@ -79,37 +81,129 @@ class _HomePageState extends State<HomePage> {
                     ),
                     SizedBox(height: 16),
                     Text(
-                      AuthenticationService.verifiedUser.branchName[0]
-                              .toUpperCase() +
-                          AuthenticationService.verifiedUser.branchName
-                              .substring(1)
-                              .toLowerCase() +
-                          ' İşletmesi',
+                      AuthenticationService.verifiedUser.branchName[0].toUpperCase() + AuthenticationService.verifiedUser.branchName.substring(1).toLowerCase() + ' İşletmesi',
                       style: TextStyle(fontSize: 18, color: Color(0xff4BADBB)),
                     ),
-                  ],
-                ),
-                Container(
-                  child: GestureDetector(
-                    onTap: () => AuthenticationService.instance.signOut(),
-                    child: Row(
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Column(
                       mainAxisSize: MainAxisSize.min,
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: <Widget>[
-                        Icon(
-                          Icons.power_settings_new,
-                          color: Theme.of(context).primaryColorDark,
+                        MenuButton(
+                          text: "Görevlerim",
+                          icon: Icons.assistant_photo,
+                          count: 3,
+                          click: () => AuthenticationService.instance.signOut(),
                         ),
-                        Text("Çıkış")
+                        MenuButton(
+                            text: "Önerilerim",
+                            icon: Icons.question_answer,
+                            click: () => Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => SuggestionPage(),
+                                  ),
+                                )),
+                        MenuButton(
+                          text: "KVKK Onayı",
+                          icon: Icons.insert_drive_file,
+                          click: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SuggestionPage(),
+                            ),
+                          ),
+                        ),
                       ],
-                    ),
-                  ),
+                    )
+                  ],
                 ),
+                MenuButton(text: "Çıkış", icon: Icons.power_settings_new, click: () => AuthenticationService.instance.signOut()),
               ],
             ),
           ),
         );
       }),
+    );
+  }
+}
+
+class MenuButton extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final Function click;
+  final int count;
+  const MenuButton({
+    this.text,
+    this.icon,
+    this.click,
+    this.count,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          padding: EdgeInsets.only(left: 30, top: 20),
+          child: GestureDetector(
+            onTap: () => click(),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  icon,
+                  color: Theme.of(context).primaryColorDark,
+                  size: 26,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text(
+                    text,
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        if (count != null)
+          Positioned(
+            right: 50,
+            top: 2,
+            child: Container(
+              width: 22,
+              height: 22,
+              padding: EdgeInsets.all(3),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              child: Center(
+                child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Text(
+                    count.toString(),
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    textWidthBasis: TextWidthBasis.longestLine,
+                    overflow: TextOverflow.clip,
+                    softWrap: false,
+                  ),
+                ),
+              ),
+            ),
+          )
+      ],
     );
   }
 }
