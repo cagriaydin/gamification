08a657a3514f27ae8118699a92a565a63a72dd97
onatcipli <onatcipli@outlook.com>
2020-05-18 01:43:13 +0300

    reward page added

---
+++ lib/pages/rewards_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RewardsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Text('reward page'),
+      ),
+    );
+  }
+}
