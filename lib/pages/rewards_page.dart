dc4a78aa59ecc325fa047355d997498c35c9aa5e
onatcipli <onatcipli@outlook.com>
2020-05-17 12:45:47 +0300

    add sticky widget

--- lib/pages/rewards_page.dart
+++ lib/pages/rewards_page.dart
@@ -24,7 +24,35 @@ class RewardsPage extends StatefulWidget {
 }
 
 class _RewardsPageState extends State<RewardsPage> with AfterLayoutMixin {
-  List<String> myList = [];
+  List<String> myList = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ];
 
   Size customPaintSize;
 
@@ -35,6 +63,8 @@ class _RewardsPageState extends State<RewardsPage> with AfterLayoutMixin {
   ValueNotifier<Offset> offsetNotifier = ValueNotifier<Offset>(Offset.zero);
 
   List<Offset> currentOffsets = [];
+
+  ScrollController controller = ScrollController();
 
   @override
   void initState() {
@@ -61,14 +91,13 @@ class _RewardsPageState extends State<RewardsPage> with AfterLayoutMixin {
       appBar: AppBar(),
       floatingActionButton: FloatingActionButton(onPressed: () {
         setState(() {
-          myList.add('value');
-        });
-        setState(() {
-          final offset = currentOffsets.elementAt(myList.length);
-          offsetNotifier.value = offset;
+          //final offset = currentOffsets.elementAt(currentOffsets.length - 1);
+          final offset = currentOffsets.elementAt(4);
+          offsetNotifier.value = offset ?? Offset.zero;
         });
       }),
       body: SingleChildScrollView(
+        controller: controller,
         child: Container(
           key: customPaintKey,
           child: CustomPaint(
@@ -108,20 +137,26 @@ class _RewardsPageState extends State<RewardsPage> with AfterLayoutMixin {
     final keyContext = customPaintKey.currentContext;
     if (keyContext != null) {
       // widget is visible
-      final box = keyContext.findRenderObject() as RenderBox;
-      final Offset pos = box.localToGlobal(offsetNotifier.value);
       //overlayOffset = pos;
-      return AnimatedBuilder(
-        animation: offsetNotifier,
-        builder: (BuildContext context, Widget child) {
-          return Positioned(
-            top: pos.dy,
-            left: pos.dx,
-            child: Container(
-              width: 50,
-              height: 50,
-              color: Colors.black,
-            ),
+      return ValueListenableBuilder(
+        valueListenable: offsetNotifier,
+        builder: (BuildContext context, Offset value, Widget child) {
+          return AnimatedBuilder(
+            animation: controller,
+            builder: (BuildContext context, Widget child) {
+              final box = keyContext.findRenderObject() as RenderBox;
+              final Offset pos = box.localToGlobal(value);
+              //final top = box.size.height - 2000;
+              return Positioned(
+                top: pos.dy - 25,
+                left: pos.dx,
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.black,
+                ),
+              );
+            },
           );
         },
       );
@@ -185,6 +220,6 @@ class MyCustomPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
+    return (oldDelegate as MyCustomPainter).list.length != this.list.length;
   }
 }
