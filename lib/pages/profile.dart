0bb02aa11a7a915e0177ba679cf8ac2d05d7d347
Mustafa Berkay <berkaymustafa03@gmail.com>
2020-05-25 02:54:15 +0300

    ui fixes 14

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -309,7 +309,7 @@ class BuildProfileInfo extends StatelessWidget {
                   child: BuildUserInfo(
                     showPercentage: true,
                     user: user,
-                    radius: size.height < 700 ? 50 : 60,
+                    radius: size.height < 700 ? 45 : 55,
                   ),
                 ),
                 FittedBox(
